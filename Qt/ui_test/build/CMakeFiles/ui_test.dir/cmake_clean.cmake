6dc49aa895e0cbaeb7d475ad6c546001d5ff55b6
romyfish <57869895@qq.com>
2023-04-17 19:17:42 +0100

    Upload the independent test for Qt part
    The Qt project without the dish detect and face recognition functions

---
+++ Qt/ui_test/build/CMakeFiles/ui_test.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ui_test_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/ui_test_autogen.dir/ParseCache.txt"
+  "ui_test_autogen"
+  "CMakeFiles/ui_test.dir/main.cpp.o"
+  "CMakeFiles/ui_test.dir/ui_test_autogen/EWIEGA46WW/qrc_pic.cpp.o"
+  "CMakeFiles/ui_test.dir/ui_test_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/ui_test.dir/widget.cpp.o"
+  "ui_test"
+  "ui_test.pdb"
+  "ui_test_autogen/EWIEGA46WW/qrc_pic.cpp"
+  "ui_widget.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ui_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
